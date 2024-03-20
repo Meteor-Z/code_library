d52939ffaf59cf0c74970fb406a61860f5c10da8
lzc <3030218083@qq.com>
2024-03-20 23:25:52 +0800

    feat: update file and main.cc

--- main.cc
+++ main.cc
@@ -1,26 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include "template/fibonacci.h"
 
-struct Node {
-    Node() { std::cout << "构造函数" << std::endl; }
-    ~Node() { std::cout << "析构函数" << std::endl; }
-    Node(const Node&) { std::cout << "赋值构造" << std::endl; }
-    Node(Node&&) { std::cout << "移动构造" << std::endl; }
-    Node& operator=(const Node&) {
-        std::cout << "拷贝复制" << std::endl;
-        return *this;
-    }
-    Node& operator=(Node&&) {
-        std::cout << "移动复制" << std::endl;
-        return *this;
-    }
-};
 int main() {
-
-    Node a;
-    std::cout << &a << std::endl;
-
-    Node b = std::move(a);
-    Node&& c = std::move(a);
-    std::cout << &b << std::endl;
-    std::cout << &c << std::endl;
+    std::cout << Fib<4>::value << std::endl;
 }
---
+++ src/template/fibonacci.h
@@ -0,0 +1,30 @@
+/**
+ * @file fibonacci.h
+ * @author lzc (liuzechen.coder@qq.com)
+ * @brief 网上看的，使用模板在编译期计算斐波那契数列
+ * @version 0.1
+ * @date 2024-03-20
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef CODE_TEMPLATE_FIBONACCI_H
+#define CODE_TEMPLATE_FIBONACCI_H
+
+template <int N>
+struct Fib {
+    static constexpr long long value = Fib<N - 1>::value + Fib<N - 2>::value;
+};
+
+template <>
+struct Fib<0> {
+    static constexpr long long value = 1;
+};
+
+template <>
+struct Fib<1> {
+    static constexpr long long value = 1;
+};
+
+#endif
