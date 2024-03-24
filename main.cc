da73583db073b0d81b21a12a28ab180d06159abf
lzc <3030218083@qq.com>
2024-03-24 21:27:22 +0800

    doc: update README.md

--- main.cc
+++ main.cc
@@ -1,5 +1,10 @@
 #include "src/demo/epoll_demo.h"
 
 int main() {
-    epoll_demo_test();
+    Result<std::string, int> a("hello");
+    if (a.is_ok()) {
+        std::cout << a.value() << std::endl;
+    }
+    Result<std::string, int> b(1);
+    std::cout << b.error() << std::endl;
 }
